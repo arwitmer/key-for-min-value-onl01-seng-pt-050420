0e41a399aa30d75dfb8a3185a13f710e0adefec4
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-17 01:14:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,3 +4,15 @@
 def key_for_min_value(name_hash)
 
 end
+
+def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
